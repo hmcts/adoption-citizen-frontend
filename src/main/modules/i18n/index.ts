f120b61b5d1839d4756b5761a961b9fefe947a3e
Kiran Varma <varma.ap@gmail.com>
2020-03-05 16:09:09 +0100

    Add lint and fix lint issues (#31)

--- src/main/modules/i18n/index.ts
+++ src/main/modules/i18n/index.ts
@@ -1,16 +1,16 @@
-import * as postProcessor from 'i18next-sprintf-postprocessor'
-import * as middleware from 'i18next-express-middleware'
-import * as express from 'express'
-import Backend from 'i18next-node-fs-backend'
+import * as postProcessor from 'i18next-sprintf-postprocessor';
+import * as middleware from 'i18next-express-middleware';
+import * as express from 'express';
+import Backend from 'i18next-node-fs-backend';
 
-import i18next from 'i18next'
+import i18next from 'i18next';
 
 /**
  * Module that enables i18n support for Express.js applications
  */
 export class I18Next {
 
-  static enableFor (app: express.Express) {
+  static enableFor (app: express.Express) { // eslint-disable-line
     i18next
       .use(Backend)
       .use(postProcessor)
@@ -18,22 +18,22 @@ export class I18Next {
       .init({
         backend: {
           loadPath: __dirname + '/../../locales/{{lng}}/{{ns}}.json',
-          addPath: __dirname + '/../../locales/{{lng}}/{{ns}}.missing.json'
+          addPath: __dirname + '/../../locales/{{lng}}/{{ns}}.missing.json',
         },
         detection: {
           order: ['querystring', 'cookie'],
           lookupQuerystring: 'lang',
           lookupCookie: 'lang',
-          caches: ['cookie']
+          caches: ['cookie'],
         },
         interpolation: {
-          escapeValue: false // Escaping is already handled by Nunjucks
+          escapeValue: false, // Escaping is already handled by Nunjucks
         },
         fallbackLng: 'en',
         preload: ['en', 'cy'],
-        saveMissing: true
-      })
-    app.use(middleware.handle(i18next))
-    return i18next
+        saveMissing: true,
+      });
+    app.use(middleware.handle(i18next));
+    return i18next;
   }
 }
--- src/test/routes/home.ts
+++ src/test/routes/home.ts
@@ -1,7 +1,7 @@
-import { expect } from 'chai'
-import request from 'supertest'
+import { expect } from 'chai';
+import request from 'supertest';
 
-import { app } from '../../main/app'
+import { app } from '../../main/app';
 
 // TODO: replace this sample test with proper route tests for your application
 describe('Home page', () => {
@@ -9,7 +9,7 @@ describe('Home page', () => {
     it('should return sample home page', async () => {
       await request(app)
         .get('/')
-        .expect((res) => expect(res.status).to.equal(200))
-    })
-  })
-})
+        .expect((res) => expect(res.status).to.equal(200));
+    });
+  });
+});
