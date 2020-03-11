456eaae81184952663decab582c5b96a7b8cf56c
Kiran Varma <varma.ap@gmail.com>
2020-03-11 15:43:59 +0000

    remove paths file

--- src/main/app/case/paths.ts
+++
@@ -1,6 +0,0 @@
-// import { RoutablePath } from 'common/router/routablePath';
-
-export class Paths {
-  // Remove below path but all new paths should go in this file
-  // static readonly homePage = new RoutablePath('/', false)
-}
