f22a300228531997e87582c0da038c3f81ff6da9
Kiran Varma <varma.ap@gmail.com>
2020-03-11 11:06:25 +0000

    remove unused variable

--- src/test/http-mocks/idam/idam.ts
+++ src/test/http-mocks/idam/idam.ts
@@ -4,7 +4,6 @@ import * as HttpStatus from 'http-status-codes';
 import mock from 'nock';
 
 const apiServiceBaseURL: string = config.get<string>('idam.api.url');
-const s2sAuthServiceBaseURL = config.get<string>('idam.service-2-service-auth.url');
 
 export const defaultAccessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpZGFtIiwiaWF0IjoxNDgzMjI4ODAwLCJleHAiOjQxMDI0NDQ4MDAsImF1ZCI6ImFkb3B0aW9uIiwic3ViIjoiYWRvcHRpb24ifQ.0EKZpjflxgaOryKryWVgXpsfJT1zTZAHM0Qfyn2-X1Q';
 
