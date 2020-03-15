0b949eafe17f7b4f214d128c079e5a369e0ecda3
Kiran Varma <varma.ap@gmail.com>
2020-03-15 21:38:59 +0000

    formatting

--- src/main/app/case/routes/task-list.ts
+++ src/main/app/case/routes/task-list.ts
@@ -4,18 +4,18 @@ import { Paths } from 'case/paths';
 
 const router = express.Router();
 
-export default router.get(
-  Paths.taskListPage.uri,
-  async (req, res) => {
+export default router
+  .get(Paths.taskListPage.uri,
+    async (req, res) => {
 
-    res.render(Paths.taskListPage.associatedView, {
-      tasks: {
-        applicantDetails: SubTaskListBuilder.buildApplicationDetailsSubTasks(),
-        yourDetails: SubTaskListBuilder.buildYourDetailsSubTasks(),
-        childDetails: SubTaskListBuilder.buildChildDetailsSubTasks(),
-        adoptionContacts: SubTaskListBuilder.buildAdoptionContactsSubTasks(),
-        declarePayments: SubTaskListBuilder.buildDeclarePaymentsSubTasks(),
-        reviewApplication: SubTaskListBuilder.buildReviewApplicationSubTasks(),
-      },
+      res.render(Paths.taskListPage.associatedView, {
+        tasks: {
+          applicantDetails: SubTaskListBuilder.buildApplicationDetailsSubTasks(),
+          yourDetails: SubTaskListBuilder.buildYourDetailsSubTasks(),
+          childDetails: SubTaskListBuilder.buildChildDetailsSubTasks(),
+          adoptionContacts: SubTaskListBuilder.buildAdoptionContactsSubTasks(),
+          declarePayments: SubTaskListBuilder.buildDeclarePaymentsSubTasks(),
+          reviewApplication: SubTaskListBuilder.buildReviewApplicationSubTasks(),
+        },
+      });
     });
-  });
