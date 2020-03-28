39662886ec6c3fa0ea28620eca6d1ce203c8cdbc
Kiran Varma <varma.ap@gmail.com>
2020-03-28 21:48:14 +0000

    subTaskListBuilder refactor

--- src/main/app/case/util/subTaskListBuilder.ts
+++ src/main/app/case/util/subTaskListBuilder.ts
@@ -1,53 +1,63 @@
 import { SubTask, TaskStatus } from '../types';
+import * as taskList from 'main/locales/en/taskList.json';
 
 export class SubTaskListBuilder {
+
   static buildApplicationDetailsSubTasks(): SubTask[] {
+    const applicationDetailSubTasks = taskList.taskItems.applicationDetails.subTasks;
+
     return [
-      new SubTask('taskList:taskItems.applicationDetails.subTasks.numOfChildren', '#', TaskStatus.Complete),
-      new SubTask('taskList:taskItems.applicationDetails.subTasks.chooseFamilyCourt', '#'),
+      new SubTask(applicationDetailSubTasks.numOfChildren, '#', TaskStatus.Complete),
+      new SubTask(applicationDetailSubTasks.chooseFamilyCourt, '#'),
     ];
   }
 
   static buildYourDetailsSubTasks(): SubTask[] {
+    const yourDetailsSubTasks = taskList.taskItems.yourDetails.subTasks;
+
     return [
-      new SubTask('taskList:taskItems.yourDetails.subTasks.personalDetails', '#'),
-      new SubTask('taskList:taskItems.yourDetails.subTasks.contactDetails', '#'),
-      new SubTask('taskList:taskItems.yourDetails.subTasks.relationshipDetails', '#'),
-      new SubTask('taskList:taskItems.yourDetails.subTasks.relationshipDetailsOnlyApplicant', '#'),
-      new SubTask('taskList:taskItems.yourDetails.subTasks.uploadDocuments', '#'),
+      new SubTask(yourDetailsSubTasks.personalDetails, '#'),
+      new SubTask(yourDetailsSubTasks.contactDetails, '#'),
+      new SubTask(yourDetailsSubTasks.relationshipDetails, '#'),
+      new SubTask(yourDetailsSubTasks.relationshipDetailsOnlyApplicant, '#'),
+      new SubTask(yourDetailsSubTasks.uploadDocuments, '#'),
     ];
   }
 
   static buildChildDetailsSubTasks(): SubTask[] {
+    const childDetailsSubTasks = taskList.taskItems.childDetails.subTasks;
+
     return [
-      new SubTask('taskList:taskItems.childDetails.subTasks.birthCertficate', '#'),
-      new SubTask('taskList:taskItems.childDetails.subTasks.adoptionCertificate', '#'),
-      new SubTask('taskList:taskItems.childDetails.subTasks.history', '#'),
-      new SubTask('taskList:taskItems.childDetails.subTasks.placementOrderDetails', '#'),
-      new SubTask('taskList:taskItems.childDetails.subTasks.birthMotherDetails', '#'),
-      new SubTask('taskList:taskItems.childDetails.subTasks.birthFatherDetails', '#'),
-      new SubTask('taskList:taskItems.childDetails.subTasks.previousCourtOrder', '#'),
-      new SubTask('taskList:taskItems.childDetails.subTasks.courtOrderDetails', '#'),
+      new SubTask(childDetailsSubTasks.birthCertficate, '#'),
+      new SubTask(childDetailsSubTasks.adoptionCertificate, '#'),
+      new SubTask(childDetailsSubTasks.history, '#'),
+      new SubTask(childDetailsSubTasks.placementOrderDetails, '#'),
+      new SubTask(childDetailsSubTasks.birthMotherDetails, '#'),
+      new SubTask(childDetailsSubTasks.birthFatherDetails, '#'),
+      new SubTask(childDetailsSubTasks.previousCourtOrder, '#'),
+      new SubTask(childDetailsSubTasks.courtOrderDetails, '#'),
     ];
   }
 
   static buildAdoptionContactsSubTasks(): SubTask[] {
+    const adoptionContactsSubTasks = taskList.taskItems.adoptionContacts.subTasks;
+
     return [
-      new SubTask('taskList:taskItems.adoptionContacts.subTasks.socialWorker', '#'),
-      new SubTask('taskList:taskItems.adoptionContacts.subTasks.childsSocialWorker', '#'),
-      new SubTask('taskList:taskItems.adoptionContacts.subTasks.solictor', '#'),
+      new SubTask(adoptionContactsSubTasks.socialWorker, '#'),
+      new SubTask(adoptionContactsSubTasks.childsSocialWorker, '#'),
+      new SubTask(adoptionContactsSubTasks.solictor, '#'),
     ];
   }
 
   static buildDeclarePaymentsSubTasks(): SubTask[] {
     return [
-      new SubTask('taskList:taskItems.declarePayments.subTasks.declarePayments', '#'),
+      new SubTask(taskList.taskItems.declarePayments.subTasks.declarePayments, '#'),
     ];
   }
 
   static buildReviewApplicationSubTasks(): SubTask[] {
     return [
-      new SubTask('taskList:taskItems.reviewApplication.subTasks.reviewApplication', '#'),
+      new SubTask(taskList.taskItems.reviewApplication.subTasks.reviewApplication, '#'),
     ];
   }
 }
