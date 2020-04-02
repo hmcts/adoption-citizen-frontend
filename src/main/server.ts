8cc9bf708927353bd43da36d904f90273bcceca4
Kiran Varma <varma.ap@gmail.com>
2020-04-02 15:37:51 +0100

    reorder imports in server.ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 import * as config from 'config';
-
 import * as secrets from 'modules/secrets';
-secrets.setup(config);
-const { Logger } = require('@hmcts/nodejs-logging');
 import * as fs from 'fs';
 import * as https from 'https';
 import * as path from 'path';
+
+secrets.setup(config);
+
 import { app } from './app';
 
+const { Logger } = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('server');
-
-// TODO: set the right port for your application
 const port: number = parseInt(process.env.PORT, 10) || 3000;
 
 if (app.locals.ENV === 'development') {
